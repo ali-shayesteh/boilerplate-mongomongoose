8e99f85c320e7f6bf5ac91b06228b76cd7a34b54
Ali Shayesteh <ali.shayesteh@gmail.com>
2024-05-11 09:19:37 +0000

    sdf

--- myApp.js
+++ myApp.js
@@ -5,7 +5,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 var Person;
 
-const personSchema = new mongoose.Schema({
+let personSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true
