a30095ce0e5255d98a4c39ba515cdd7c7620cdc4
Ali Shayesteh <ali.shayesteh@gmail.com>
2024-05-11 09:17:18 +0000

    debg

--- myApp.js
+++ myApp.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+var Person;
+
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -12,7 +14,7 @@ const personSchema = new mongoose.Schema({
   favoriteFoods: String
 });
 
-const Person = mongoose.model('Person', personSchema);
+Person = mongoose.model('Person', personSchema);
 
 
 const createAndSavePerson = (done) => {
