f69f49d5f881efe1251a7b4ddc9e90e1c32e18db
Ali Shayesteh <ali.shayesteh@gmail.com>
2024-05-11 06:39:07 +0000

    dfg

--- myApp.js
+++ myApp.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
